d6210b2c7813cffdcbc6e3c24387ae9d13c24385
ibrahim salawu <53803785+bobbyzano@users.noreply.github.com>
2020-02-05 12:05:13 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class generate_pin(Resource):
 
 
 
-api.add_resource(generate_pin, '/') 
+api.add_resource(generate_pin, '/api/generate') 
 
 class validate_pin(Resource):
     def get(self, sn):
@@ -67,7 +67,7 @@ class validate_pin(Resource):
             return{'message':'invalid pin'}
         
 
-api.add_resource(validate_pin, '/<string:sn>')         
+api.add_resource(validate_pin, '/api/generate/validate/<string:sn>')         
     
 
 
